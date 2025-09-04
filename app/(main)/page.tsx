b1657ee01c224dbf05609f6aaeb26a611bd42e27
Koo Kuu <vinhtrungngo1907@gmail.com>
2025-09-04 10:53:53 +0700

    first commit

---
+++ app/(main)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+	return (
+		<div>
+		<p className="text-3xl font-bold text-indigo-500">Hello Coders X Tokyo</p>
+		<Button >
+			Click Me
+		</Button>
+		</div>
+	);
+}
