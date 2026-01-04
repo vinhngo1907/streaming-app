efa962a95cfc919a0776a0c443ec23129234252d
vinhngo1907 <vinhtrungngo1907@gmail.com>
2026-01-04 09:55:24 +0700

    feat(master): add some files

---
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -0,0 +1,10 @@
+export default function ServerIdLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <div className="h-full">
+            <div className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
+                Server sidebar
+            </div>
+            <main className="h-full md:pl-60">{children}</main>
+        </div>
+    )
+}
--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 export default function SetupPage () {
     return (
         <div className="h-full flex flex-col space-y-4 items-center justify-content text-muted-foreground">
