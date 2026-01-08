44404c784429faa883b55a646f86256933eb9155
vinhngo1907 <vinhtrungngo1907@gmail.com>
2026-01-08 15:47:49 +0700

    ❤️

--- app/api/channels/[channelId]/route.ts
+++ app/api/channels/[channelId]/route.ts
@@ -3,40 +3,35 @@ import { currentProfile } from "@/lib/initial-profile";
 import { MemberRole } from "@prisma/client";
 import { NextResponse } from "next/server";
 
-export async function POST(req: Request) {
+export async function PATCH(req: Request,
+    { params }: { params: { channelId: string } }
+) {
     try {
         const profile = await currentProfile();
         if (!profile) return new NextResponse("Unauthorized", { status: 401 });
 
         const { searchParams } = new URL(req.url);
         const serverId = searchParams.get("serverId");
-        if (!serverId) return new NextResponse("Server ID is Missing: ", { status: 400 });
+        if (!serverId) return new NextResponse("Server ID Missing", { status: 400 });
+        if (!params.channelId) return new NextResponse("Channel ID Missing", { status: 400 });
 
         const { name, type } = await req.json();
-        if (name === "general") return new NextResponse("Name cannot be 'general'", { status: 400 });
+        if (!name || !type) return new NextResponse("Name / Type cannot be empty or general", { status: 400 });
 
         const server = await db.server.update({
-            where: {
-                id: serverId,
-                members: {
-                    some: {
-                        profileId: profile.id,
-                        role: { in: [MemberRole.ADMIN, MemberRole.MODERATOR] }
-                    }
-                }
-            },
+            where: { id: serverId, members: { some: { profileId: profile.id, role: { in: [MemberRole.ADMIN, MemberRole.MODERATOR] } } } },
             data: {
                 channels: {
-                    create: {
-                        profileId: profile.id, name, type
+                    update: {
+                        where: { id: params.channelId, NOT: { name: "general" } },
+                        data: { name, type }
                     }
                 }
             }
         });
-
         return NextResponse.json(server);
     } catch (error) {
-        console.error("[CHANNELS_POST", error);
+        console.error("[CHANNEL_ID_PATCH", error);
         return new NextResponse("Internal Error", { status: 500 });
     }
 }
